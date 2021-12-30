29712f27d4c4da0f41d4ffb8dd81c952861803e2
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-30 16:35:17 +0200

    create a test file for the counter button component

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -2,7 +2,7 @@ import { shallow } from "enzyme";
 import React from "react";
 import CardList from "./CardList";
 
-it('expect to render Card component', () => {
+it('expect to render Card List component', () => {
     const mockRobots = [
         {
             id: 2,
